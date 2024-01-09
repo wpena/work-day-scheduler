8aa8bbb6458992d1475d88dfdaa351823de59a8c
Pena <penawalin@gmail.com>
2024-01-09 22:34:12 +0000

    Created function to check and update time block color according to the current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
 // Functions are only called once all DOM elements are ready to be used
-$(function() {
-    // Displays the current date
+$(function () {
+    // Displays the current date and time
     const todayDate = dayjs().format('dddd, MMMM DD YYYY [ | Time: ] h:mma')
     $('#currentDay').text(todayDate);
+
+    // Checks and updates time block colors
+    function updateTimeBlock() {
+
+        let currentHour = dayjs().hour();
+
+        $('.time-block').each(function () {
+            // Splits to get id
+            let hourBlock = parseInt($(this).attr('id').split('-')[1]);
+
+            // Compares the hour id to the current time
+            if (hourBlock < currentHour) {
+                $(this).addClass('past');
+            } else if (hourBlock === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past present');
+                $(this).addClass('future');
+            }
+        });
+    }
+
+    updateTimeBlock();
 });
